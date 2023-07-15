5b88ee7cb824249f90a90370db1b43415656769c
vinay1506 <vinaykumarkasanaboina@gmail.com>
2023-07-15 16:00:40 +0530

    Add project files.

---
+++ Models/Domain/BlogPostcs.cs
@@ -0,0 +1,16 @@
+﻿namespace Bloggie.Models.Domain
+{
+    public class BlogPostcs
+    {
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string URLHandle { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string ShortDescription { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public bool Visible { get; set; }
+    }
+}
